39e3fbe6311fa7fdac6270e5fe166cdc145e4ec8
YamaMaru15 <liberte1la1vie7@gmail.com>
2023-09-13 10:29:04 +0000

    first commit

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP_TEST</title>
+    </head>
+    <body>
+        <h1>データ受け取りプログラム</h1>
+        <p>お名前を入力してください</p>
+        <form action="html/result.php" method="post">
+            <input type="text" name="data">
+            <input type="submit" name="datapost">
+        </form>
+
+    </body>
+</html>
---
+++ html/result.php
@@ -0,0 +1,15 @@
+<?php
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP_TEST</title>
+    </head>
+    <body>
+        <h1>データを受け取りました。</h1>
+        <p><?php echo $_POST["data"]; ?></p>
+    </body>
+</html>
